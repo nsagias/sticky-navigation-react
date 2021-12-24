2b6414a4a9e584f250c1afe44eeaf2110c8eaa41
Nick <nsagias@gmail.com>
2021-12-24 16:43:17 -0500

    import boilerplate code

--- sticky-navigation-react/src/components/LandingPage.tsx
+++ sticky-navigation-react/src/components/LandingPage.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 
 export default function LandingPage() {
   return (
-    <h1>this is landing page</h1>
+    <body>
+    <div className="hero">
+      <div className="container">
+        <h1>Welcome to A Company Website</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, possimus.</p>
+      </div>
+    </div>
+
+    <section className="container content">
+      <h2>Paragraph One</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus natus ab distinctio odio. Exercitationem ad vel possimus dolore in, incidunt blanditiis minus unde voluptatem quaerat quae numquam ratione velit ut dolor voluptas beatae temporibus obcaecati provident deserunt repellat. Voluptas beatae dolore tempore ullam molestias sed voluptate voluptatibus, ducimus provident distinctio deserunt laboriosam optio, consequuntur necessitatibus! Velit in quisquam earum voluptatibus nihil dolorum corporis, enim dolore laudantium rerum voluptates inventore! Reprehenderit officia maxime explicabo ratione delectus. Harum, unde ea reiciendis, nihil deleniti earum voluptate rerum numquam in assumenda illo molestias amet. Placeat magnam a consequuntur voluptatibus! Quisquam officiis suscipit quod minima.</p>
+      <h3>Paragraph Two</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione necessitatibus quo repudiandae officia laboriosam nulla sed iure ducimus. Animi, voluptatem ipsa veniam sequi praesentium repellendus nihil sint excepturi, ratione nobis voluptate in suscipit et officiis perferendis esse eligendi obcaecati? Repudiandae nisi at, veniam sit pariatur corrupti soluta molestiae nihil ipsum, eaque ut odio modi. Dolor culpa, reprehenderit aliquid labore cum deserunt explicabo corrupti expedita, officia adipisci consequatur quasi! Maiores omnis, debitis aliquid consequuntur placeat et deleniti, perspiciatis quibusdam iure doloremque facilis? Quidem ratione harum voluptatum maiores ipsam dicta accusantium impedit eaque quo quaerat nam illum rerum, id voluptate dolorem nesciunt.</p>
+    </section>
+
+    </body>
   )
 }
