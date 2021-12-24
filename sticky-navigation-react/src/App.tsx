170d34e906ab33dac9269be902036a310330a45a
Nick <nsagias@gmail.com>
2021-12-24 16:38:56 -0500

    import nav css app

--- sticky-navigation-react/src/App.tsx
+++ sticky-navigation-react/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Nav.css';
 import LandingPage from './components/LandingPage';
 import NavigationMenu from './components/NavigationMenu';
 
