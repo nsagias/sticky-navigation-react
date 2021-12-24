ff086f2b170326d094f284eab5aa4692a3784482
Nick <nsagias@gmail.com>
2021-12-24 16:45:21 -0500

    Fix tag levels

--- sticky-navigation-react/src/components/NavigationMenu.tsx
+++ sticky-navigation-react/src/components/NavigationMenu.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 export default function NavigationMenu() {
   return (
     <nav className="nav">
-    <div className="container">
-      <h1 className="logo"><a href="/index.html">A Company</a></h1>
-      <ul>
-        <li><a href="#" className="current">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </div>
-  </nav>
+      <div className="container">
+        <h1 className="logo"><a href="/index.html">A Company</a></h1>
+        <ul>
+          <li><a href="#" className="current">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
   )
 }
