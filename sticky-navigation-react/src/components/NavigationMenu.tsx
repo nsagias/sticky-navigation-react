50d9a7377e7b7ec8a027f18f8705c2d8672e9277
Nick <nsagias@gmail.com>
2021-12-24 18:48:07 -0500

    Change navigationPagesList data structure add font colour

--- sticky-navigation-react/src/components/NavigationMenu.tsx
+++ sticky-navigation-react/src/components/NavigationMenu.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 export default function NavigationMenu() {
 
   const navigationPagesList = [
-    {id: 1, pageName: "Home", pageLink: "/"},
-    {id: 2, pageName: "About", pageLink: "about"},
-    {id: 3, pageName: "Services", pageLink: "services"},
-    {id: 4, pageName: "Contact", pageLink: "contact"},
-    {id: 5, pageName: "Login", pageLink: "Login"}
+    {id: 1, pageName: "Home", pageLink: "/", fontColor: ""},
+    {id: 2, pageName: "About", pageLink: "about", fontColor: ""},
+    {id: 3, pageName: "Services", pageLink: "services", fontColor: ""},
+    {id: 4, pageName: "Contact", pageLink: "contact", fontColor: ""},
+    {id: 5, pageName: "Login", pageLink: "Login", fontColor: ""},
   ];
   
   const parsedNavigationPages = Array.isArray(navigationPagesList) && navigationPagesList.map(navigationPage => (
