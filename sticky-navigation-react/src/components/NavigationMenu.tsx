fa082f14b24ea1aa86568ad76241ac1c80d0a555
Nick <nsagias@gmail.com>
2021-12-24 20:06:22 -0500

    Add inline code to mouseover

--- sticky-navigation-react/src/components/NavigationMenu.tsx
+++ sticky-navigation-react/src/components/NavigationMenu.tsx
@@ -4,17 +4,32 @@ export default function NavigationMenu() {
 
   const navigationPagesList = [
     {id: 1, pageName: "Home", pageLink: "/", fontColor: ""},
-    {id: 2, pageName: "About", pageLink: "about", fontColor: ""},
-    {id: 3, pageName: "Services", pageLink: "services", fontColor: ""},
+    {id: 2, pageName: "About", pageLink: "about", fontColor: "#307B7F"},
+    {id: 3, pageName: "Services", pageLink: "services", fontColor: "orange"},
     {id: 4, pageName: "Contact", pageLink: "contact", fontColor: ""},
     {id: 5, pageName: "Login", pageLink: "Login", fontColor: ""},
   ];
   
+
   const parsedNavigationPages = Array.isArray(navigationPagesList) && navigationPagesList.map(navigationPage => (
     <li key={navigationPage.id}>
-      <a href={navigationPage.pageLink}>{navigationPage.pageName}</a>
+      <a 
+        className={navigationPage.pageName}
+        href={navigationPage.pageLink} 
+        onMouseOver={(event:any)=>{event.target.style.color = "red"}}
+        onMouseLeave={(event:any)=>{event.target.style.color = navigationPage.fontColor}}
+        style={{color: navigationPage.fontColor}}
+        
+        >{navigationPage.pageName}</a>
     </li>
   ));
+
+  // function onHoverFontColor(event: any) {
+  //   event.target.style.color = "green";
+  // }
+  // function onHoverFontColorLeave(event: any) {
+  //   event.target.style.color = "#fff";
+  // }
 
   return (
     <nav className="nav">
