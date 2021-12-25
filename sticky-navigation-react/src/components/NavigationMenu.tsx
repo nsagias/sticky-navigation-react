77f90b810bb79e49e5492b8b9c20c667990065be
Nick <nsagias@gmail.com>
2021-12-24 20:48:53 -0500

    add constant colour to pages and mouserover

--- sticky-navigation-react/src/components/NavigationMenu.tsx
+++ sticky-navigation-react/src/components/NavigationMenu.tsx
@@ -2,31 +2,35 @@ import React from 'react';
 
 export default function NavigationMenu() {
 
+  const NAV_HOME_COLOR:     string = "#C2185B";
+  const NAV_ABOUT_COLOR:    string = "#1976D2";
+  const NAV_SERVICES_COLOR: string = "#F57C00";
+  const NAV_CONTACT_COLOR:  string = "#327F62";
+  const NAV_LOGIN_COLOUR:   string = "#fff";
+ 
   const navigationPagesList = [
-    {id: 1, pageName: "Home", pageLink: "/", fontColor: ""},
-    {id: 2, pageName: "About", pageLink: "about", fontColor: "#307B7F"},
-    {id: 3, pageName: "Services", pageLink: "services", fontColor: "orange"},
-    {id: 4, pageName: "Contact", pageLink: "contact", fontColor: ""},
-    {id: 5, pageName: "Login", pageLink: "Login", fontColor: ""},
+    {id: 1, pageName: "Home", pageLink: "/", fontColor: NAV_HOME_COLOR},
+    {id: 2, pageName: "About", pageLink: "about", fontColor: NAV_ABOUT_COLOR},
+    {id: 3, pageName: "Services", pageLink: "services", fontColor:  NAV_SERVICES_COLOR},
+    {id: 4, pageName: "Contact", pageLink: "contact", fontColor: NAV_CONTACT_COLOR},
+    {id: 5, pageName: "Login", pageLink: "Login", fontColor: NAV_LOGIN_COLOUR},
   ];
   
+  const NAV_LOGIN_MOUSE_OVER: string = "#D500F9"
 
   const parsedNavigationPages = Array.isArray(navigationPagesList) && navigationPagesList.map(navigationPage => (
     <li key={navigationPage.id}>
       <a 
         className={navigationPage.pageName}
         href={navigationPage.pageLink} 
-        onMouseOver={(event:any)=>{event.target.style.color = "red"}}
+        onMouseOver={(event:any)=>{event.target.style.color = NAV_LOGIN_MOUSE_OVER}}
         onMouseLeave={(event:any)=>{event.target.style.color = navigationPage.fontColor}}
         style={{color: navigationPage.fontColor}}
-        
         >{navigationPage.pageName}</a>
     </li>
   ));
 
-  // function onHoverFontColor(event: any) {
-  //   event.target.style.color = "green";
-  // }
+
   // function onHoverFontColorLeave(event: any) {
   //   event.target.style.color = "#fff";
   // }
