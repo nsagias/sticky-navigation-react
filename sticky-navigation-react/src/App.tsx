f46ffd201f5d052d51245ed9840aa1b3bd4f17e3
Nick <nsagias@gmail.com>
2021-12-25 15:43:34 -0500

    add navContainer

--- sticky-navigation-react/src/App.tsx
+++ sticky-navigation-react/src/App.tsx
@@ -84,6 +84,13 @@ const styles = {
     right: 0,
     transition: "all 0.3s ease-in-out",
   },
+  navContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "20px 0",
+    transition: "all 0.3s ease-in-out",
+  },
   hero: {
     backgroundImage: "url('https://images.unsplash.com/photo-1578104053615-4d7664bd6364?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80')",
     backgroundRepeat: "no-repeat",
